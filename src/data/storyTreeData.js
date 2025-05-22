ded6eeac2ba9ac4b5f5130b5425157637cb48689
George <georgekutsia@gmail.com>
2025-05-22 23:22:00 +0100

    añadiendo audio

--- src/data/storyTreeData.js
+++ src/data/storyTreeData.js
@@ -1,23 +1,23 @@
 const storyTree = {
   start: {
     id: 'start',
-    sound: '',
+    // sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949130/Lea1.L_z8juqi.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949780/SIlencioso_lkhjhj.mp3',
     image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747862469/Leandro-perfil_jmwtj0.png',
-    options: [
-      { text: 'Ir al bosque', nextId: 'bosque' },
-      { text: 'Seguir el camino', nextId: 'camino' }
-    ]
   },
   options1: [
     {
-      id: 'vestirUniforme',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      // id: 'vestirUniforme',
+      id: 'aLaClase',
+      // sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949150/Lea2.L_vlthvw.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949780/SIlencioso_lkhjhj.mp3',
+
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866031/option0-a_dao1rr.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866103/option0-resa_xh7a5i.png'
     },
     {
-      id: 'construir', //hay qeu cambiar a vestirBombero
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404829/JifiAu14.R_nilq9a.wav',
+      id: 'vestirBombero', 
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949150/Lea2.L_vlthvw.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866035/option0-b_z8aqzp.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866103/option0-resb_v183on.png'
     },
@@ -26,19 +26,19 @@ const storyTree = {
   vestirUniforme: [
     {
       id: 'mascara',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404829/JifiAu14.R_nilq9a.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747950499/lea3r_TKqkr57z_uvoxat.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866615/option2-a_pwfrfp.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866960/option3-d_bga17b.png'
     },
     {
       id: 'alCole',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404829/JifiAu14.R_nilq9a.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949157/Lea6.L_d3uisr.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866611/option2-b_rjbart.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747870139/option3-res_hjjjvn.png'
     },
     {
       id: 'camion',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747950500/lea4l_ocGwM1JT_szt7a6.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866784/option2-c_y2gq5t.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866959/option3-f_kkkeey.png'
     }
@@ -46,19 +46,19 @@ const storyTree = {
   mascara:[
     {
       id: 'alCole',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949157/Lea6.L_d3uisr.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747871039/opt4-a_aa51ot.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747870139/option3-res_hjjjvn.png'
     },
     {
       id: 'alCole',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949157/Lea6.L_d3uisr.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747871375/opt4-d_xxuawk.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747870139/option3-res_hjjjvn.png'
     },
     {
       id: 'alCole',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949157/Lea6.L_d3uisr.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747871040/opt4-c_hem2fi.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747870139/option3-res_hjjjvn.png'
     },
@@ -66,19 +66,19 @@ const storyTree = {
   camion:[
     {
       id: 'alCole',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949155/Lea7.L_cbimpi.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747871039/opt4-a_aa51ot.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747870139/option3-res_hjjjvn.png'
     },
     {
       id: 'alCole',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949155/Lea7.L_cbimpi.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747871375/opt4-d_xxuawk.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747870139/option3-res_hjjjvn.png'
     },
     {
       id: 'alCole',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949155/Lea7.L_cbimpi.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747871040/opt4-c_hem2fi.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747870139/option3-res_hjjjvn.png'
     },
@@ -87,19 +87,19 @@ const storyTree = {
   vestirBombero: [
     {
       id: 'cambiarRopa',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404829/JifiAu14.R_nilq9a.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949155/Lea7.L_cbimpi.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866615/option2-a_pwfrfp.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866881/option3-a_vdt6fx.png'
     },
     {
       id: 'cambiarRopa',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404829/JifiAu14.R_nilq9a.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949155/Lea7.L_cbimpi.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866611/option2-b_rjbart.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866960/option3-b_edutrv.png'
     },
     {
       id: 'cambiarRopa',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747949155/Lea7.L_cbimpi.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866784/option2-c_y2gq5t.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747866959/option3-c_ftljsp.png'
     }
@@ -108,43 +108,39 @@ const storyTree = {
   cambiarRopa: [
     {
       id: 'alCole',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404829/JifiAu14.R_nilq9a.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747950947/Lea8.L_zdpq8x.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747867330/option4-c_vqrnx2.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747870139/option3-res_hjjjvn.png',
     },
     {
       id: 'alCole',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404829/JifiAu14.R_nilq9a.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747950947/Lea8.L_zdpq8x.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747867330/option4-d_robcvk.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747870139/option3-res_hjjjvn.png',
     },
     {
       id: 'alCole',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747950947/Lea8.L_zdpq8x.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747867328/option4-b_xv5tbd.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747870139/option3-res_hjjjvn.png',
     },
     {
       id: 'alCole',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747950947/Lea8.L_zdpq8x.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747867326/option4-a_z46389.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747870139/option3-res_hjjjvn.png',
     }
   ],
-
-
-
-  
   alCole:[
     {
       id: 'alParque',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound:'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747951358/Lea9.L_gbksvk.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747872163/ChatGPT_Image_22_may_2025_01_01_12_tjtyjr.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747872442/opt5-res_aabrkv.png',
     },
     {
       id: 'aLaClase',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747951408/Lea10.L_zd1ksr.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747872215/option5-a_tl7wid.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747873036/option7-a_u2gxah.png',
     }
@@ -153,13 +149,13 @@ const storyTree = {
   alParque:[
     {
       id: 'aLaClase',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747951408/Lea10.L_zd1ksr.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747872524/option6-a_lb0rky.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747872780/ChatGPT_Image_22_may_2025_01_12_49_xuy2pb.png',
     },
     {
       id: 'aLaClase',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747951408/Lea10.L_zd1ksr.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747872529/option6-b_onh3yl.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747872780/ChatGPT_Image_22_may_2025_01_12_49_xuy2pb.png',
     }
@@ -167,19 +163,19 @@ const storyTree = {
   aLaClase:[
     {
       id: 'construir',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747952042/lea13-peppa_3nRuuQHA_ur7ort.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747873228/pot7-c_n6lqmx.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747873036/option7-a_u2gxah.png',
     },
     {
       id: 'construir',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747952044/lea13-superleonr_C4D4ldv6_n2ecar.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747873228/opt7-a_jmyxa5.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747873036/option7-a_u2gxah.png',
     },
     {
       id: 'construir',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747952045/lea13-marshall_XXNKoKbE_xddoyn.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747873228/opt7-b_jjsi5r.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747873036/option7-a_u2gxah.png',
     }
@@ -187,19 +183,19 @@ const storyTree = {
   construir:[
     {
       id: 'libros',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747952208/Lea14-legos.L_qpd29w.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747873411/opt8-a_jd1xqp.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747874072/opt8-res_puxvlu.png',
     },
     {
       id: 'libros',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747952210/Lea14-bloques.R_vtkm7z.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747873415/opt8-b_sskzbb.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747874072/opt8-res_puxvlu.png',
     },
     {
       id: 'libros',
-      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+      sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747952214/Lea14-pintar.R_staeds.wav',
       image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747873415/0pt8-c_cwlha5.png',
       displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747874072/opt8-res_puxvlu.png',
     },
@@ -207,19 +203,19 @@ const storyTree = {
 libros:[
   {
     id: 'comedor',
-    sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+    sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747952209/Lea15-dinos.L_i3stzf.wav',
     image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747874652/opt9-a_ivpxyl.png',
     displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747875262/opt9-resa_biyzoi.png',
   },
   {
     id: 'comedor',
-    sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+    sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747952340/Lea15-artico.L_giykvk.wav',
     image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747874652/opt9-c_df01ce.png',
     displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747875331/op9-resb_tdzntb.png',
   },
   {
     id: 'comedor',
-    sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+    sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747952342/Lea15-cocina.R_e9mzkc.wav',
     image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747874652/opt9-b_bdhyzs.png',
     displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747875716/opt9-resc_oovwbu.png',
   },
@@ -228,25 +224,25 @@ libros:[
 comedor:[
   {
     id: 'parqueElige',
-    sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+    sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747952480/Lea16-fruta.L_join_tklgef.mp3',
     image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747875685/op10-b_bqf7vz.png',
     displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747876621/opt10-resb_qaiihh.png',
   },
   {
     id: 'parqueElige',
-    sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+    sound: '',
     image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747875677/op10-d_emkl4c.png',
     displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747876621/opt10-resa_tn4je6.png',
   },
   {
     id: 'parqueElige',
-    sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+    sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1747952446/Lea16-ensalada.L_join_pmkmih.mp3',
     image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747875684/op10-c_exgmsb.png',
     displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747876621/opt10-resb_qaiihh.png',
   },
   {
     id: 'parqueElige',
-    sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+    sound: '',
     image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747875684/op10-a_ro6s7r.png',
     displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747876621/opt10-resa_tn4je6.png',
   },
@@ -254,7 +250,7 @@ comedor:[
 parqueElige:[
   {
     id: 'vueltaAClase',
-    sound: 'https://res.cloudinary.com/dtv1oj9bq/video/upload/v1740404849/JifiAu2.R_lgp1z3.wav',
+    sound: '',
     image: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747876033/opt11-b_hqdqow.png',
     displayImage: 'https://res.cloudinary.com/dtv1oj9bq/image/upload/v1747876342/opt11-resb_zomptf.png',
   },
